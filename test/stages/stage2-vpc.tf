6e2615a502f7d60d60fb2c5166bbecdfd2e36324
Sean Sundberg <seansund@us.ibm.com>
2022-02-17 14:42:02 -0800

    Updates validation logic
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-vpc.tf
+++ test/stages/stage2-vpc.tf
@@ -8,3 +8,9 @@ module "vpc" {
   address_prefixes    = ["10.0.0.0/16"]
   enabled             = var.enabled
 }
+
+resource null_resource print_enabled {
+  provisioner "local-exec" {
+    command = "echo -n '${module.resource_group.enabled}' > .enabled"
+  }
+}
