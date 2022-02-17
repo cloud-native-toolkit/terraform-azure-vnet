5a76936283c718e36cf592864610e300c4d2ac89
Sean Sundberg <seansund@us.ibm.com>
2022-02-17 14:48:59 -0800

    Updates metadata and readme
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-resource-group.tf
+++ test/stages/stage1-resource-group.tf
@@ -3,4 +3,5 @@ module "resource_group" {
 
   resource_group_name = var.resource_group_name
   region              = var.region
+  enabled             = var.enabled
 }
--- test/stages/stage2-vpc.tf
+++ test/stages/stage2-vpc.tf
@@ -6,7 +6,7 @@ module "vpc" {
   name_prefix         = var.name_prefix
   address_prefix_count = 1
   address_prefixes    = ["10.0.0.0/16"]
-  enabled             = var.enabled
+  enabled             = module.resource_group.enabled
 }
 
 resource null_resource print_enabled {
