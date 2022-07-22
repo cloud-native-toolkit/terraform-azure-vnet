a28add23299ed28e7ff3e6c8fbc77e2c02d3d8e0
Rich Ehrhardt <51330294+rich-ehrhardt@users.noreply.github.com>
2022-07-22 13:46:20 +1000

    12 use random name for GitHub testing to avoid conflicts with other testing (#14)
    * Added random name for github testing
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Removed boilerplate content from readme
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>

--- test/stages/stage1-resource-group.tf
+++ test/stages/stage1-resource-group.tf
@@ -1,6 +1,12 @@
+resource "random_string" "cluster_id" {
+    length = 5
+    special = false
+    upper = false
+}
+
 module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-azure-resource-group"
 
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.resource_group_name}-${random_string.cluster_id.result}"
   region              = var.region
 }
